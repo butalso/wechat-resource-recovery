2c5a09e63e8a9143a18ecd5c5274fe4d95308b8e
Lavi <lan_xiao163@163.com>
2018-03-03 21:48:57 +0800

    调整数据库，对接订单查询，地址显示

--- web-consumer/src/main/webapp/static/index/customer.js
+++ web-consumer/src/main/webapp/static/index/customer.js
@@ -15,6 +15,7 @@ function init() {
         },
         success: function (data) {
             $(".head").html('您好 ' + data.nickName + '<a href="/web-consumer/logout" class="logout">登出</a>');
+            $(".address").text(data.address.province + data.address.city + data.address.area + data.address.housingEstate);
             console.log(data);
         },
         error: function (err) {
@@ -24,6 +25,7 @@ function init() {
 }
 
 function getMoreOrder() {
+    $(".bs-example").show();
     $.ajax({
         type: 'GET',
         url: LOCALHOST + "/order/info",
@@ -32,6 +34,22 @@ function getMoreOrder() {
         complete: function (XMLHttpRequest, textStatus) {
         },
         success: function (data) {
+            $orderList = $(".orderList");
+            var node = '';
+            var garbageNode = '';
+            $.each(data, function (index, element) {
+                garbageNode = '';
+                $.each(element.orderDetails, function (index, element) {
+                    garbageNode += '<td>' + element.name + ':' + element.weight + '千克</td>';
+                });
+                node = '<tr>\
+                            <td>' + element.id + '</td>\
+                            <td>' + element.stateInfo + '</td>\
+                            ' + garbageNode + '\
+                        </tr>';
+                console.log(node);
+                $orderList.append(node);
+            });
             console.log(data);
         },
         error: function (err) {
