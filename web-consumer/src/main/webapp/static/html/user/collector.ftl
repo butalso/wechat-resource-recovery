7c225bb261058fa9af8ce449605d5b2a0abddbfc
Lavi <lan_xiao163@163.com>
2018-03-10 16:23:18 +0800

    回收员接单

--- web-consumer/src/main/webapp/static/html/user/collector.ftl
+++ web-consumer/src/main/webapp/static/html/user/collector.ftl
@@ -1,10 +1,79 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <title>Title</title>
-</head>
-<body>
-<p>回收员个人中心</p>
-</body>
-</html>
+<@override name="header"></@override>
+<@override name="content">
+    <div class="regions main-grid-border">
+        <div class="container">
+            <h2 class="head">您好 许先生<a href="/web-consumer/logout" class="logout">登出</a></h2>
+        </div>
+        <div class="region-block">
+            <div class="container">
+                <div class="state col-md-3">
+                    <h3>待抢单<span class="more moreUserOrder">展示>></span></h3>
+                </div>
+                <div class="sun-regions col-md-9">
+                    <ul>
+                        <li><a href="#">待接收</a></li>
+                        <li><a href="#">待回收</a></li>
+                        <li><a href="#">已回收</a></li>
+                    </ul>
+                </div>
+                <table class="table userOrderTab" style="display: none">
+                    <thead>
+                    <tr>
+                        <th>#</th>
+                        <th>创建时间</th>
+                        <th>创建人</th>
+                        <th>创建地址</th>
+                        <th>订单内容</th>
+                    </tr>
+                    </thead>
+                    <tbody class="userOrderList">
+                    </tbody>
+                </table>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <div class="region-block">
+            <div class="container">
+                <div class="state col-md-3">
+                    <h3>我的订单<span class="more moreMyOrder">展示>></span></h3>
+                </div>
+                <div class="sun-regions col-md-9">
+                    <ul>
+                        <li><a href="#">待接收</a></li>
+                        <li><a href="#">待回收</a></li>
+                        <li><a href="#">已回收</a></li>
+                    </ul>
+                </div>
+                <table class="table myOrderTab" style="display: none;">
+                    <thead>
+                    <tr>
+                        <th>订单编号</th>
+                        <th>创建时间</th>
+                        <th>创建人</th>
+                        <th>订单内容</th>
+                    </tr>
+                    </thead>
+                    <tbody class="myOrderList">
+                    </tbody>
+                </table>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+        <div class="region-block">
+            <div class="container">
+                <div class="state col-md-3">
+                    <h3>地址管理<span class="more">更多>></span></h3>
+                </div>
+                <div class="sun-regions col-md-9">
+                    <p class="address"></p>
+                </div>
+                <div class="clearfix"></div>
+            </div>
+        </div>
+    </div>
+</@override>
+
+<@override name="js">
+<script src="/web-consumer/static/index/collector.js"></script>
+</@override>
+<@extends name="layout.ftl"/>
---
+++ web-consumer/src/main/webapp/static/index/collector.js
@@ -0,0 +1,124 @@
+$(function () {
+    init();
+    $(".moreUserOrder").click(function () {
+        getUserOrder();
+    });
+    $(".moreMyOrder").click(function () {
+        getMyOrder();
+    });
+});
+
+function init() {
+    $.ajax({
+        type: 'GET',
+        url: LOCALHOST + "/user/info/detail",
+        dataType: 'json',
+        // data: data,
+        complete: function (XMLHttpRequest, textStatus) {
+        },
+        success: function (data) {
+            $(".head").html('您好 ' + data.nickName + '<a href="/web-consumer/logout" class="logout">登出</a>');
+            $(".address").text(data.address.province + data.address.city + data.address.area + data.address.housingEstate);
+            console.log(data);
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
+
+function getUserOrder() {
+    $(".userOrderTab").fadeToggle();
+    $.ajax({
+        type: 'GET',
+        url: LOCALHOST + "/order/new",
+        dataType: 'json',
+        // data: data,
+        complete: function (XMLHttpRequest, textStatus) {
+        },
+        success: function (data) {
+            console.log(data);
+            $orderList = $(".userOrderList");
+            $orderList.empty();
+            var node = '';
+            var garbageNode = '';
+            $.each(data, function (index, element) {
+                garbageNode = '';
+                $.each(element.orderDetails, function (index, element) {
+                    garbageNode += '<td>' + element.name + ':' + element.weight + '千克</td>';
+                });
+                node = '<tr>\
+                            <td><button class="btn btn-default getOrder" value=' + element.id + '>抢单</button></td>\
+                            <td>' + element.createTime + '</td>\
+                            <td>' + element.customerName + '</td>\
+                            <td>' + element.address.province + element.address.city + element.address.area + element.address.housingEstate + '</td>\
+                            ' + garbageNode + '\
+                        </tr>';
+                $orderList.append(node);
+            });
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    }).done(function () {
+        $(".getOrder").click(function () {
+            var $this = $(this);
+            $.ajax({
+                type: 'POST',
+                url: LOCALHOST + "/order/receive",
+                // dataType: 'json',
+                data: {
+                    orderId: $this.val()
+                },
+                complete: function (XMLHttpRequest, textStatus) {
+                },
+                success: function (data) {
+                    console.log(data);
+                    if (data == "接单成功") {
+                        $this.attr('disabled', "true").text("已抢单");
+                    }
+                },
+                error: function (err) {
+                    console.log(err);
+                }
+            });
+            console.log($(this).val());
+        })
+    });
+}
+
+function getMyOrder() {
+    $(".myOrderTab").fadeToggle();
+    $.ajax({
+        type: 'GET',
+        url: LOCALHOST + "/order/info",
+        dataType: 'json',
+        // data: data,
+        complete: function (XMLHttpRequest, textStatus) {
+        },
+        success: function (data) {
+            console.log(data);
+            $orderList = $(".myOrderList");
+            $orderList.empty();
+            var node = '';
+            var garbageNode = '';
+            $.each(data, function (index, element) {
+                garbageNode = '';
+                $.each(element.orderDetails, function (index, element) {
+                    garbageNode += '<td>' + element.name + ':' + element.weight + '千克</td>';
+                });
+                node = '<tr>\
+                            <td>' + element.id + '</td>\
+                            <td>' + element.createTime + '</td>\
+                            <td>' + element.customerName + '</td>\
+                            <td>' + element.address.province + element.address.city + element.address.area + element.address.housingEstate + '</td>\
+                            ' + garbageNode + '\
+                        </tr>';
+                $orderList.append(node);
+            });
+        },
+        error: function (err) {
+            console.log(err);
+        }
+    });
+}
