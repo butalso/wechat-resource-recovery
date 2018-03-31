291f33dd42b51acd5a2169499af6ae5573182c4c
Lavi <lan_xiao163@163.com>
2018-03-31 16:46:31 +0800

    添加兑换反馈

--- web-consumer/src/main/webapp/static/index/gift.js
+++ web-consumer/src/main/webapp/static/index/gift.js
@@ -1,6 +1,6 @@
 $(function () {
     $(".getGift").click(function () {
-        console.log($(this).attr("giftId"));
+        $(this).attr("disabled", "disabled").text("已兑换");
         $.ajax({
             type: 'POST',
             url: LOCALHOST + "/gift/" + $(this).attr("giftId"),
@@ -9,7 +9,9 @@ $(function () {
             complete: function (XMLHttpRequest, textStatus) {
             },
             success: function (data) {
-                Toast("礼品将于7个工作日为您发出，请注意查收");
+                if (data == "兑换成功") {
+                    Toast("礼品将于7个工作日为您发出，请注意查收");
+                }
                 console.log(data);
             },
             error: function (err) {
